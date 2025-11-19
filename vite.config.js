88511f45d197a55ad7f2a33ac8b973e704338bab
Zahid Ali <k24sw012zahid2645khan873@gmail.com>
2025-11-19 10:43:51 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/ZahidAli-Portfolio/', // <-- important for GitHub Pages
+  base: '/ZahidAli-Portfolio/', // match the repo name exactly
   plugins: [react()],
 })
